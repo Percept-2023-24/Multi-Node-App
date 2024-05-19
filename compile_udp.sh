c6119f0b24f438e3bea5f9cd31d88273e1979f0d
Aditya Ramakrishnan <adityarules2013@gmail.com>
2024-05-18 20:15:27 -0700

    modified visualizer

--- compile_udp.sh
+++ compile_udp.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-g++ -o node1 node1.cpp
-g++ -o node2 node2.cpp
-g++ -o server server.cpp
 g++ -o tcp_server tcp_server.cpp
+g++ -o visualizer visualizer.cpp `pkg-config --cflags --libs opencv4`
